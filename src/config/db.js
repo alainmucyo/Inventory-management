127c2befb4bb3120fa3de7725d430fbed455ca5f
Gilbert Elnino <gilbeltelnino@gmail.com>
2020-10-09 17:30:04 +0200

    fix token sign

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,11 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
+    
     database: process.env.DB_DATABASE,
     host: "127.0.0.1",
     dialect: process.env.DB_TYPE,
+    
   },
   test: {
     username: process.env.DB_USERNAME,
